6eccb28e818b54a3d8948f19bcac01995e0e2e6b
asok <adam.sokolnicki@gmail.com>
2015-01-27 22:28:57 +0100

    improv: redirect non-relevant output from wkhtmltopdf to null

--- app.rb
+++ app.rb
@@ -9,5 +9,5 @@ end
 post '/' do
   response['Content-Type']        = 'application/pdf'
   response['Content-Disposition'] = 'inline'
-  `echo "#{erb(:pdf, layout: false)}" | bundle exec wkhtmltopdf --encoding utf8 - -`
+  `echo "#{erb(:pdf, layout: false)}" | bundle exec wkhtmltopdf --encoding utf8 - - 2>/dev/null`
 end
