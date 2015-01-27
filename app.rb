1e166f791a613637ffaf57d587b83aa45f5bfe0f
asok <adam.sokolnicki@gmail.com>
2015-01-27 15:16:32 +0100

    feature: create a simple template and render it to pdf

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+  response['Content-Type']        = 'application/pdf'
+  response['Content-Disposition'] = 'inline'
+  `echo "#{erb(:pdf, layout: false)}" | wkhtmltopdf --encoding utf8 - -`
+end
